968d017f01863973bf734fcfd89f202f69147dd7
NebulaTen <abdullahniyas171@gmail.com>
2026-06-21 19:40:24 +0400

    Create supabase.js

---
+++ site/js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+    "https://piypvksqfgiyfnzyrkuo.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpeXB2a3NxZmdpeWZuenlya3VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMzc4MjQsImV4cCI6MjA5NzYxMzgyNH0.HQbniop3_X-L8udkJKhpovA37uCOP9oETz8ppr2nK-w"
+);
