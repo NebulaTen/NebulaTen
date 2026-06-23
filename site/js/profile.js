858c9a0156ee8669bbb67b826a43a4d5ffa3705f
Fyve <186026883+Fyve505@users.noreply.github.com>
2026-06-23 15:42:49 +0400

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -1,1 +1,77 @@
+import { supabase } from "./supabase.js";
 
+const {
+    data:{session}
+} = await supabase.auth.getSession();
+
+if(!session){
+
+    location.href="login.html";
+
+    throw new Error("Not logged in");
+
+}
+
+const user = session.user;
+
+const usernameEl =
+document.getElementById("username");
+
+const emailEl =
+document.getElementById("email");
+
+const shardsEl =
+document.getElementById("shards");
+
+const joinedEl =
+document.getElementById("joined");
+
+const avatarEl =
+document.getElementById("avatar");
+
+const logoutBtn =
+document.getElementById("logoutBtn");
+
+emailEl.textContent =
+user.email;
+
+const { data,error } =
+await supabase
+.from("profiles")
+.select("*")
+.eq("id",user.id)
+.single();
+
+if(error){
+
+    console.error(error);
+
+}else{
+
+    usernameEl.textContent =
+    data.username;
+
+    shardsEl.textContent =
+    data.shards;
+
+    joinedEl.textContent =
+    new Date(
+        data.created_at
+    ).toLocaleDateString();
+
+    if(data.avatar_url){
+
+        avatarEl.src =
+        data.avatar_url;
+
+    }
+
+}
+
+logoutBtn.onclick = async()=>{
+
+    await supabase.auth.signOut();
+
+    location.href="index.html";
+
+};
